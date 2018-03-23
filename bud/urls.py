c1c2f36bcd55047d87c041f5b3bfa1e5fb7bdbe1
Canjiang Lu <canjiang.lu@samsung.com>
2018-03-23 14:03:34 +0800

    [Split][Detail View]init

--- bud/urls.py
+++ bud/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from account_views import *
 from task_views import *
 from transaction_views import *
+from split_views import *
 
 urlpatterns = [
     url(r'^account/$', AccountListView.as_view(), name='account_index'),
@@ -14,4 +15,5 @@ urlpatterns = [
     url(r'^task/create/$', TaskCreateView.as_view(), name='task_create'),
     url(r'^task/(?P<pk>[\d]+)/$', TaskDetailView.as_view(), name='task_detail'),
     url(r'^transaction/(?P<pk>[\d]+)/$', TransactionDetailView.as_view(), name='transaction_detail'),
+    url(r'^split/(?P<pk>[\d]+)/$', SplitDetailView.as_view(), name='split_detail'),
 ]
