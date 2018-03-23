282c78347729799e5713d52adf9059b6506481d6
Canjiang Lu <canjiang.lu@samsung.com>
2018-03-23 14:28:47 +0800

    [Transaction][Update View]+splits

--- bud/transaction_views.py
+++ bud/transaction_views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from .models import *
 from django.views.generic import DetailView
 from django.views.generic import UpdateView
+from django.forms import ModelForm
+from django.forms import modelformset_factory
 
 class TransactionDetailView(DetailView):
 	model = Transaction
@@ -13,7 +15,14 @@ class TransactionDetailView(DetailView):
 		self.object.splits = self.object.split_set.all()
 		return context
 
+SplitFormSet = modelformset_factory(Split, fields = ("account", "change"))
+
 class TransactionUpdateView(UpdateView):
 	model = Transaction
 	fields = ['desc']
 	template_name_suffix = '_update_form'
+
+	def get_context_data(self, **kwargs):
+		context = super(TransactionUpdateView, self).get_context_data(**kwargs)
+		self.object.splits = SplitFormSet(queryset=self.object.split_set.all())
+		return context
