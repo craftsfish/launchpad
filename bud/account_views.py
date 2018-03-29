b10e6336e15e31f883ffe7e1213993912e6d4ed9
Canjiang Lu <canjiang.lu@samsung.com>
2018-03-29 15:40:57 +0800

    [Account][Detail View]display all descendants

--- bud/account_views.py
+++ bud/account_views.py
@@ -28,11 +28,11 @@ class AccountDetailView(DetailView):
 	def get_context_data(self, **kwargs):
 		context = super(AccountDetailView, self).get_context_data(**kwargs)
 
-		#children
-		children = []
-		for p in self.object.paths2descendant.order_by("height").filter(height=1):
-			children.append(p.descendant)
-		context['children'] = children
+		#descendants
+		descendants = []
+		for p in self.object.paths2descendant.filter(height__gte=1).order_by("height"):
+			descendants.append(p.descendant)
+		context['descendants'] = descendants
 
 		#ancestors
 		ancestors = []
