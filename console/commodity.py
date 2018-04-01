170f0f7e2058601c843ed739b6dea428eae8038e
Canjiang Lu <craftsfish@163.com>
2018-04-01 17:13:24 +0800

    trivial test

---
+++ console/commodity.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+class Test:
+	def __init__(self):
+		print "aaa"
--- console/main.py
+++ console/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
 from bud.models import *
+from commodity import *
 
 class Console:
 	@staticmethod
 	def run():
 		print Supplier.objects.all()
+		Test()
