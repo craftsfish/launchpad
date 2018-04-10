2bb2e2a6de6766c1ba4c7128787765a373bc5059
Canjiang Lu <canjiang.lu@samsung.com>
2018-04-10 18:26:04 +0800

    [Supplier]+abbrev

--- bud/models.py
+++ bud/models.py
@@ -7,6 +7,7 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 class Supplier(models.Model):
 	name = models.CharField(max_length=30, unique=True)
+	abbrev = models.CharField(max_length=10, unique=True)
 
 	def __str__(self):
 		return self.name
