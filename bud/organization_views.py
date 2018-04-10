be43d60c4adbe9e3c23f9f61a72193cf46db47c4
Canjiang Lu <canjiang.lu@samsung.com>
2018-04-10 19:29:50 +0800

    [Organization][DetailView]init

--- bud/organization_views.py
+++ bud/organization_views.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from .models import *
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 class OrganizationListView(ListView):
+	model = Organization
+
+class OrganizationDetailView(DetailView):
 	model = Organization
