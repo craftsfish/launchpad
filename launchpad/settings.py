638094f2726a2f687e1e41c65e4afd15bfe49361
Canjiang Lu <canjiang.lu@samsung.com>
2018-04-11 10:38:29 +0800

    [Organization]model init

--- launchpad/settings.py
+++ launchpad/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'bud.apps.BudConfig',
+    'bud',
+    'vault',
 ]
 
 MIDDLEWARE = [
--- vault/models.py
+++ vault/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+	name = models.CharField(max_length=30)
+	parent = models.ForeignKey('self', verbose_name="上级", null=True, blank=True, related_name="children")
+
+	def __str__(self):
+		return self.name
