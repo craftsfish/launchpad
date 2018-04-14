9f900b23d19757b93f24bd4572d6da29a3b59c9b
Canjiang Lu <craftsfish@163.com>
2018-04-14 19:29:26 +0800

    [Organization][DetailView]+sub-organizations

--- vault/organization_views.py
+++ vault/organization_views.py
@@ -14,12 +14,11 @@ class OrganizationDetailView(DetailView):
 	def get_context_data(self, **kwargs):
 		context = super(OrganizationDetailView, self).get_context_data(**kwargs)
 
-		self.object.descendants()
 		def __get_cell(matrix, item, category):
 			r = matrix.get(item)
 			if r == None:
 				item_name = Item.objects.get(pk=item).name
-				matrix[item] = [item_name, [0, [], []], [0, [], []], [0, [], []], [0, [], []], [0, [], []]]
+				matrix[item] = [item_name, [0, {}, []], [0, {}, []], [0, {}, []], [0, {}, []], [0, {}, []]]
 			return matrix[item][category+1]
 
 		#2-dimension (item, category) array to store account info which include each sub-organization and direct account
@@ -31,7 +30,20 @@ class OrganizationDetailView(DetailView):
 			c[0] += a.balance #accumulation of total
 			c[2].append(a)
 
-		#TODO: sub organizations
+		#sub organizations
+		def __get_org_from_cell(cell, org):
+			r = cell[1].get(org)
+			if r == None:
+				cell[1][org] = [Organization.objects.get(pk=org), 0]
+			return cell[1][org]
+
+		for o in self.object.children.all():
+			sub_orgs = [o] + o.descendants()
+			for a in Account.objects.filter(organization__in=sub_orgs):
+				c = __get_cell(matrix, a.item.id, a.category)
+				_o = __get_org_from_cell(c, o.id)
+				c[0] += a.balance
+				_o[1] += a.balance
 
 		context['matrix'] = matrix
 		return context
