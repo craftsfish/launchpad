c41f660d011312d6f2a6ca61576846b3589fb799
Canjiang Lu <craftsfish@163.com>
2018-04-14 11:22:22 +0800

    [Model]+Task,Transaction,Split

--- vault/models.py
+++ vault/models.py
@@ -50,3 +50,60 @@ class Account(models.Model):
 
 	def __str__(self):
 		return "{}.{}.{}.{}".format(str(self.organization), self.item.name, self.get_category_display(), self.name)
+
+class Task(models.Model):
+	desc = models.CharField(max_length=120)
+
+	def __str__(self):
+		return self.desc
+
+	#def get_absolute_url(self):
+		#return reverse('task_detail', kwargs={'pk': self.pk})
+
+	def delete(self, *args, **kwargs):
+		for t in self.transactions.all():
+			t.delete()
+		super(Task, self).delete(*args, **kwargs)
+
+class Transaction(models.Model):
+	desc = models.CharField(max_length=120)
+	task = models.ForeignKey(Task, null=True, blank=True, related_name="transactions")
+	time = models.DateTimeField()
+
+	def __str__(self):
+		return self.desc
+
+	#def get_absolute_url(self):
+		#return reverse('transaction_detail', kwargs={'pk': self.pk})
+
+	def delete(self, *args, **kwargs):
+		for s in self.splits.all():
+			s.delete()
+		super(Transaction, self).delete(*args, **kwargs)
+
+class Split(models.Model):
+	account = models.ForeignKey(Account)
+	change = models.DecimalField(max_digits=20, decimal_places=2)
+	transaction = models.ForeignKey(Transaction, related_name="splits")
+
+	def __str__(self):
+		return "{} {:+}".format(self.account.name, self.change)
+
+	#def get_absolute_url(self):
+		#return reverse('split_detail', kwargs={'pk': self.pk})
+
+	def save(self, *args, **kwargs):
+		if self.id:
+			orig = Split.objects.get(pk=self.id)
+			orig.account.balance -= orig.change
+			orig.account.save()
+		#reload account to reflect changes made in previous instructions
+		account = Account.objects.get(pk=self.account.id)
+		account.balance += self.change
+		account.save()
+		super(Split, self).save(*args, **kwargs)
+
+	def delete(self, *args, **kwargs):
+		self.account.balance -= self.change
+		self.account.save()
+		super(Split, self).delete(*args, **kwargs)
