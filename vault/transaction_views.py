bb4284fc6b5a2d6429c8fb04712610e91977e56e
Canjiang Lu <craftsfish@163.com>
2018-04-21 20:02:59 +0800

    [Transaction][UpdateView]use inlineformset for splits again

--- vault/transaction_views.py
+++ vault/transaction_views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from .models import *
 from django import forms
 from django.contrib.admin import widgets
+from django.forms import inlineformset_factory
 from django.views.generic import DetailView
 from django.views.generic import UpdateView
 
@@ -24,17 +25,17 @@ class TransactionDetailView(TransactionMixin, DetailView):
 		context['splits'] = self.object.splits.all()
 		return context
 
+SplitFormSet = inlineformset_factory(Transaction, Split, fields=('change', 'account'), widgets = {'account': forms.HiddenInput()}, labels = {'change': ''}, extra=0)
 class TransactionUpdateView(TransactionMixin, UpdateView):
 	def get_context_data(self, **kwargs):
 		context = super(TransactionUpdateView, self).get_context_data(**kwargs)
-		splits = self.object.splits.all()
-		for i, s in enumerate(splits):
-			s.id_id = "id_splits_{}_id".format(i)
-			s.id_account = "id_splits_{}_account".format(i)
-			s.id_change = "id_splits_{}_change".format(i)
-			s.id_delete = "id_splits_{}_delete".format(i)
-		context['splits'] = splits
+		context['formset'] = SplitFormSet(instance=self.object)
+		for f in context['formset']:
+			f.account_display_name = str(f.instance.account)
 		return context
 
 	def form_valid(self, form):
+		formset = SplitFormSet(self.request.POST, self.request.FILES, instance=self.object)
+		if formset.is_valid():
+			formset.save()
 		return super(TransactionUpdateView, self).form_valid(form)
