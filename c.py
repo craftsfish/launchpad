c6f2e7e708df9555c7cc107c348b4d1d69362394
Canjiang Lu <craftsfish@163.com>
2018-05-06 09:21:45 +0800

    [Console]+command handling framework

--- c.py
+++ c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
 import os
 import sys
 
@@ -12,8 +13,34 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "launchpad.settings")
 django.setup()
 
 #add your codes here
+import traceback
 from vault.item import *
 from vault.jd_commodity import *
 from vault.jd_order import *
-#Item.Import()
-Jdorder.Import()
+
+options = (
+	["ii", "导入物资", Item.Import],
+	["ijdo", "导入京东订单", Jdorder.Import],
+)
+
+def command_handling():
+	op = raw_input("请输入命令:")
+	for key, desc, func in options:
+		if key == op:
+			func()
+			return
+
+	#invalid option, list commands
+	for key, desc, func in options:
+		print("{}: {}".format(key, desc))
+
+try:
+	while True:
+		command_handling()
+		print "----------------------------------------任务分割线----------------------------------------"
+except (EOFError, KeyboardInterrupt) as e: #ctrl+d & ctrl+c
+	print "再见!"
+except Exception, e:
+	traceback.print_exc()
+
+print "End!"
