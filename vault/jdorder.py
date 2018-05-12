653cd631affefa007170db5d74e959633621950a
Canjiang Lu <craftsfish@163.com>
2018-05-12 14:06:15 +0800

    import jdorder

--- vault/jdorder.py
+++ vault/jdorder.py
@@ -7,10 +7,10 @@ from ground import *
 from jdcommodity import *
 
 class Jdtransaction:
-    pass
+	pass
 
 class Jdinvoice:
-    pass
+	pass
 
 class Jdorder(models.Model):
 	id = models.BigIntegerField("订单编号", primary_key=True)
@@ -27,22 +27,47 @@ class Jdorder(models.Model):
 
 	@staticmethod
 	def Import():
-		#Jdcommodity mapping information check
-		exit = False
-		with open('/tmp/jd.csv', 'rb') as csvfile:
-			reader = csv.reader(csv_gb18030_2_utf8(csvfile))
-			title = reader.next()
-			for l in reader:
-				jdcid = int(get_column_value(title, l, "商品ID"))
-				try:
-					jdc = Jdcommodity.objects.get(id=jdcid)
-				except Jdcommodity.DoesNotExist as e:
-					jdc = Jdcommodity(id=jdcid)
-					jdc.save()
-				booktime = utc_2_datetime(cst_2_utc(get_column_value(title, l, "下单时间"), "%Y-%m-%d %H:%M:%S"))
-				items = Jdcommoditymap.get(jdc, booktime)
-				if items == None:
-					exit = True
-					print "{}) {}:{} 缺乏商品信息".format(booktime.astimezone(timezone.get_current_timezone()), jdc, get_column_value(title, l, "商品名称"))
-		if exit:
-			return
+		def __mapping_check():
+			with open('/tmp/jd.csv', 'rb') as csvfile:
+				result = True
+				reader = csv.reader(csv_gb18030_2_utf8(csvfile))
+				title = reader.next()
+				for l in reader:
+					jdcid = int(get_column_value(title, l, "商品ID"))
+					try:
+						jdc = Jdcommodity.objects.get(id=jdcid)
+					except Jdcommodity.DoesNotExist as e:
+						jdc = Jdcommodity(id=jdcid)
+						jdc.save()
+					booktime = utc_2_datetime(cst_2_utc(get_column_value(title, l, "下单时间"), "%Y-%m-%d %H:%M:%S"))
+					items = Jdcommoditymap.get(jdc, booktime)
+					if items == None:
+						result = False
+						print "{}) {}:{} 缺乏商品信息".format(booktime.astimezone(timezone.get_current_timezone()), jdc, get_column_value(title, l, "商品名称"))
+			return result
+
+		def __import():
+			ts = []
+			with open('/tmp/jd.csv', 'rb') as csvfile:
+				reader = csv.reader(csv_gb18030_2_utf8(csvfile))
+				title = reader.next()
+				for l in reader:
+					found = False
+					order_id = int(get_column_value(title, l, "订单号"))
+					for t in ts:
+						if t.id == order_id:
+							found = True
+							break
+					if not found:
+						t = Jdtransaction()
+						t.id = order_id
+						t.sale = float(get_column_value(title, l, "应付金额"))
+						t.remark = get_column_value(title, l, "商家备注")
+						t.booktime = utc_2_datetime(cst_2_utc(get_column_value(title, l, "下单时间"), "%Y-%m-%d %H:%M:%S"))
+						t.invoices = []
+						ts.append(t)
+						print "发现新订单: {} {} {} {}".format(t.id, t.sale, t.remark, t.booktime)
+
+		if not __mapping_check():
+			print "请完善商品映射信息后重试!"
+		__import()
