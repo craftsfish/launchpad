211f141fb19aaa87e9c8e56337975b90f8923cff
Canjiang Lu <craftsfish@163.com>
2018-05-12 22:59:07 +0800

    [Item][DetailView]init

--- vault/item_views.py
+++ vault/item_views.py
@@ -3,7 +3,15 @@ from __future__ import unicode_literals
 
 from .models import *
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 class ItemListView(ListView):
 	model = Item
 	paginate_by = 100
+
+class ItemDetailView(DetailView):
+	model = Item
+
+	def get_context_data(self, **kwargs):
+		context = super(ItemDetailView, self).get_context_data(**kwargs)
+		return context
--- vault/urls.py
+++ vault/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
 
 	#item
 	url(r'^item/$', ItemListView.as_view(), name='item_list'),
+	url(r'^item/(?P<pk>[\d]+)/$', ItemDetailView.as_view(), name='item_detail'),
 
 	#account
 	url(r'^account/$', AccountListView.as_view(), name='account_list'),
