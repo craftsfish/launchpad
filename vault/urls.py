066fe3cdb88591283bbadb4b416a2cc02dcef915
Canjiang Lu <craftsfish@163.com>
2018-05-13 15:31:32 +0800

    clean up

--- vault/urls.py
+++ vault/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from organization_views import *
 from supplier_views import *
 from item_views import *
 from account_views import *
@@ -7,10 +6,6 @@ from transaction_views import *
 from jdcommodity_views import *
 
 urlpatterns = [
-	#organization
-	url(r'^organization/$', OrganizationListView.as_view(), name='organization_list'),
-	url(r'^organization/(?P<pk>[\d]+)/$', OrganizationDetailView.as_view(), name='organization_detail'),
-
 	#supplier
 	url(r'^supplier/$', SupplierListView.as_view(), name='supplier_list'),
 
