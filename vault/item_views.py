2e79b53bbd9a74d2ecf162d21f52818e972828c4
Canjiang Lu <craftsfish@163.com>
2018-05-13 15:03:21 +0800

    [Item][DetailView]+sub organizations

--- vault/item_views.py
+++ vault/item_views.py
@@ -29,15 +29,33 @@ class ItemDetailView(DetailView):
 		context = super(ItemDetailView, self).get_context_data(**kwargs)
 		context['org'] = self.org
 		total = [0, 0, 0, 0, 0]
-		#orgs = [{}, {}, {}, {}, {}]
+		orgs = [{}, {}, {}, {}, {}]
 		accounts = [[], [], [], [], []]
 
 		#direct monitored accounts
+		has_account = False
 		for a in Account.objects.filter(item=self.object).filter(organization=self.org).order_by("category", "name"):
 			c = a.category
 			total[c] += a.balance
 			accounts[c].append(a)
-		context['accounts'] = accounts
-		context['total'] = total
+			has_account = True
+		if has_account:
+			context['accounts'] = accounts
 
+		#sub organizations
+		has_orgs = False
+		for o in self.org.children.all():
+			sub_orgs = [o] + o.descendants()
+			for a in Account.objects.filter(item=self.object).filter(organization__in=sub_orgs):
+				c = a.category
+				total[c] += a.balance
+				if orgs[c].get(o.id) == None:
+					orgs[c][o.id] = [o, 0]
+				orgs[c][o.id][1] += a.balance
+				has_orgs = True
+		if has_orgs:
+			context['orgs'] = orgs
+
+		#total
+		context['total'] = total
 		return context
