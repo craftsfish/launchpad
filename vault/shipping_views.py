d4e0ca5503000a30ec5b97b614d40cf87d776e09
Canjiang Lu <canjiang.lu@samsung.com>
2018-05-19 16:19:56 +0800

    re-construction

--- vault/shipping_views.py
+++ vault/shipping_views.py
@@ -30,12 +30,28 @@ class ShippingForm(forms.Form):
 				return v
 		return None
 
+def get_sign(i):
+	if i < 0:
+		return "-"
+	return "+"
+
+def purchase(task, time, organization, item, quantity, repository, status):
+	cash = Item.objects.get(name="人民币")
+	account = ("资产", "应收")
+	if repository:
+		account = ("资产", "在库")
+	task.add_transaction("进货("+get_sign(quantity)+")", time, organization, item, account, quantity, ("收入", "进货"))
+	task.add_transaction("货款("+get_sign(quantity)+")", time, organization, cash, ("负债", "应付货款"), quantity*item.value, ("支出", "进货"))
+	if repository:
+		task.add_transaction("收货("+get_sign(quantity)+")", time, repository, item, ("资产", status), quantity, ("收入", "收货"))
+
 class ShippingInCreateView(FormView):
 	template_name = "{}/shipping_form.html".format(Organization._meta.app_label)
 	form_class = ShippingForm
 	TITLES = ("进货", "出货", "收货", "发货")
 
 	def post(self, request, *args, **kwargs):
+		self.shipping_type = int(kwargs['type'])
 		self.task = Task.objects.get(pk=kwargs['task_id'])
 		p = self.request.POST
 		o = Organization.objects.get(pk=p['organization'])
@@ -47,14 +63,7 @@ class ShippingInCreateView(FormView):
 			if p.get("invoice_{}_include".format(i)) == "on":
 				it = Item.objects.get(pk=p["invoice_{}_item".format(i)])
 				q = int(p["invoice_{}_quantity".format(i)])
-				if r:
-					self.task.add_transaction("进货", timezone.now(), o, it, ("资产", "在库"), q, ("收入", "进货"))
-				else:
-					self.task.add_transaction("进货", timezone.now(), o, it, ("资产", "应收"), q, ("收入", "进货"))
-				self.task.add_transaction("货款", timezone.now(), o, Item.objects.get(name="人民币"), ("负债", "应付货款"), q*it.value, ("支出", "进货"))
-				if r: #收货
-					self.task.add_transaction("收货", timezone.now(), r, it, ("资产", s), q, ("收入", "收货"))
-
+				purchase(self.task, timezone.now(), o, it, q, r, s)
 		return super(ShippingInCreateView, self).post(request, *args, **kwargs)
 
 	def get_success_url(self):
