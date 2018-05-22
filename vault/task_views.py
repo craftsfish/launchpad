b42a613fe664a16e83a93dcc6c777650e33cc5a7
Canjiang Lu <canjiang.lu@samsung.com>
2018-05-22 19:25:46 +0800

    [Task]+receive_future

--- vault/task_views.py
+++ vault/task_views.py
@@ -53,8 +53,8 @@ class TaskBuyFutureView(FormView):
 				q = int(p["invoice_{}_quantity".format(i)])
 				cash = Item.objects.get(name="人民币")
 				t = timezone.now()
-				self.task.add_transaction("进货.{}".format(it.name), t, o, it, ("资产", "应收"), q, ("收入", "进货"))
-				self.task.add_transaction("货款.{}".format(it.name), t, o, cash, ("负债", "应付货款"), q*it.value, ("支出", "进货"))
+				self.task.add_transaction("进货", t, o, it, ("资产", "应收"), q, ("收入", "进货"))
+				self.task.add_transaction("货款", t, o, cash, ("负债", "应付货款"), q*it.value, ("支出", "进货"))
 		return super(TaskBuyFutureView, self).post(request, *args, **kwargs)
 
 	def get_success_url(self):
@@ -68,4 +68,61 @@ class TaskBuyFutureView(FormView):
 			j.name_item = "invoice_{}_item".format(i)
 			j.name_quantity = "invoice_{}_quantity".format(i)
 			j.step = 3
+		return context
+
+class ReceiveForm(forms.Form):
+	o = Organization.objects.get(name="企业")
+	os = []
+	for i in o.descendants():
+		os.append(i.id)
+	r = Organization.objects.get(name="仓库")
+	rs = []
+	for i in r.descendants():
+		rs.append(i.id)
+	organization = forms.ModelChoiceField(queryset=Organization.objects.filter(id__in=os))
+	repository = forms.ModelChoiceField(queryset=Organization.objects.filter(id__in=rs), required=False)
+	ITEM_STATUS_CHOICES = (
+		(0, "完好"),
+		(1, "残缺"),
+		(2, "破损"),
+	)
+	status = forms.ChoiceField(choices=ITEM_STATUS_CHOICES)
+
+	@staticmethod
+	def status_2_str(s):
+		for i, v in ReceiveForm.ITEM_STATUS_CHOICES:
+			if i == s:
+				return v
+		return None
+
+class TaskReceiveFutureView(FormView):
+	template_name = "{}/receive_future.html".format(Organization._meta.app_label)
+	form_class = ReceiveForm
+
+	def post(self, request, *args, **kwargs):
+		self.task = Task.objects.get(pk=kwargs['pk'])
+		p = self.request.POST
+		o = Organization.objects.get(pk=p['organization'])
+		r = Organization.objects.get(pk=p['repository'])
+		s = ReceiveForm.status_2_str(int(p.get('status')))
+		for i in range(int(p['items_total'])):
+			if p.get("invoice_{}_include".format(i)) == "on":
+				it = Item.objects.get(pk=p["invoice_{}_item".format(i)])
+				q = int(p["invoice_{}_quantity".format(i)])
+				t = timezone.now()
+				self.task.add_transaction("收货", t, o, it, ("资产", "应收"), -q, ("资产", "在库"))
+				self.task.add_transaction("入库", t, r, it, ("资产", s), q, ("收入", "收货"))
+		return super(TaskReceiveFutureView, self).post(request, *args, **kwargs)
+
+	def get_success_url(self):
+		return self.task.get_absolute_url()
+
+	def get_context_data(self, **kwargs):
+		context = super(TaskReceiveFutureView, self).get_context_data(**kwargs)
+		context['items'] = Item.objects.all()
+		for i, j in enumerate(context['items']):
+			j.name_check = "invoice_{}_include".format(i)
+			j.name_item = "invoice_{}_item".format(i)
+			j.name_quantity = "invoice_{}_quantity".format(i)
+			j.step = 1
 		return context
