7d471fc11f887573b9fda081c7ddffb53e5c45f7
Canjiang Lu <craftsfish@163.com>
2018-05-27 10:10:10 +0800

    [jdorder]repository information takes into consideration

--- vault/jdorder.py
+++ vault/jdorder.py
@@ -47,10 +47,28 @@ class Jdorder(models.Model):
 
 	@staticmethod
 	def Import():
-		def __jdorder_shipping_out_future(task, info, organization):
-			for i in info.invoices:
-				for item in Jdcommoditymap.get(Jdcommodity.objects.get(pk=i.id), info.booktime):
-					Shipping.future_out(task, info.booktime, organization, item, i.number)
+		#为订单中的每一条出货记录添加相应的Transaction(s)
+		def __add_shipping_transactions(task, organization, repository, info):
+			for i, v in enumerate(info.invoices):
+				for c in Jdcommoditymap.get(Jdcommodity.objects.get(pk=v.id), info.booktime):
+					if info.status == "等待出库":
+						Transaction.add(task, "{}.{}.出货".format(i+1, c.name), info.booktime, organization, c.item_ptr,
+							("负债", "应发", repository), v.number, ("支出", "出货", repository))
+					else:
+						Transaction.add(task, "{}.{}.出货".format(i+1, c.name), info.booktime, organization, c.item_ptr,
+							("资产", "完好", repository), -v.number, ("支出", "出货", repository))
+
+		#增加一条刷单Transaction
+		def __add_fake_transaction(task, organization, repository, info):
+					c = Commodity.objects.get(name="洗衣粉")
+					Transaction.add(task, "0.{}.出货".format(c.name), info.booktime, organization, c.item_ptr,
+						("资产", "完好", repository), -1, ("支出", "出货", repository))
+
+		def __add_commodity_transaction(task, organization, repository, info, fake):
+			if fake:
+				__add_fake_transaction(task, organization, repository, info)
+			else:
+				__add_shipping_transactions(task, organization, repository, info)
 
 		def __handle_transaction(info, org, repo):
 			if info.status == "锁定":
@@ -67,42 +85,38 @@ class Jdorder(models.Model):
 					return
 
 				if o.fake != f: #刷单状态变更
-					for t in o.task.transactions.filter(desc__in=["期货出货", "期货发货", "出库"]):
-						t.delete()
-					if f:
-						Shipping.future_out(o.task, info.booktime, org, Item.objects.get(name="洗衣粉"), 1)
-					else:
-						__jdorder_shipping_out_future(o.task, info, org)
-					if info.status != "等待出库":
-						task_future_deliver(o.task, repo)
-				else: #正常订单状态迁移
-					if Jdorder.str2status(info.status) != o.status:
-						task_future_deliver(o.task, repo)
+					#delete obsolete transactions and re-add
+					for i in range(len(info.invoices)+1):
+						s = "{}.".format(i)
+						for t in o.task.transactions.filter(desc__startswith=s):
+							t.delete()
+					__add_commodity_transaction(o.task, org, repo, info, f)
+				elif not f: #正常订单状态迁移
+					for i in range(len(info.invoices)):
+						s = "{}.".format(i+1)
+						for t in o.task.transactions.filter(desc__startswith=s):
+							s = t.splits.exclude(account__category=3)[0]
+							if s.account.category == 1 and info.status != "等待出库":
+								a = s.account
+								s.account = Account.get(a.organization, a.item, "资产", "完好", a.repository)
+								s.change = -s.change
+								s.save()
 
-				#TODO: 发货仓库发生变化
-
-				#更新订单状态
 				o.status = Jdorder.str2status(info.status)
 				o.fake = f
 				o.save()
-
 			except Jdorder.DoesNotExist as e:
 				t = Task(desc="京东订单")
 				t.save()
 				o = Jdorder(id=info.id, status=Jdorder.str2status(info.status), task=t, fake=f)
 				o.save()
-
 				if info.status in ["(删除)锁定", "(删除)等待出库", "(删除)等待确认收货"]:
 					return #no transaction should be added
-				t.add_transaction("出单", info.booktime, org, Item.objects.get(name="人民币"),
-					("资产", "应收账款"), info.sale, ("收入", "营业收入"))
-				if f:
-					Shipping.future_out(t, info.booktime, org, Item.objects.get(name="洗衣粉"), 1)
-				else:
-					__jdorder_shipping_out_future(t, info, org)
-				if info.status != "等待出库":
-					task_future_deliver(t, repo)
+				Transaction.add(t, "出单", info.booktime, org, Money.objects.get(name="人民币").item_ptr,
+					("资产", "应收账款", None), info.sale, ("收入", "营业收入", None))
+				__add_commodity_transaction(t, org, repo, info, f)
 
+		#Import
 		ts = []
 		with open('/tmp/jd.csv', 'rb') as csvfile:
 			reader = csv.reader(csv_gb18030_2_utf8(csvfile))
@@ -159,6 +173,7 @@ class Jdorder(models.Model):
 				t.invoices.append(invc)
 
 			org = Organization.objects.get(name="为绿厨具专营店")
-			repo = Organization.objects.get(name="孤山仓")
+			repo = Repository.objects.get(name="孤山仓")
 			for t in ts:
+				t.invoices = sorted(t.invoices, key = lambda i: (i.id * 100000 + i.number))
 				__handle_transaction(t, org, repo)
