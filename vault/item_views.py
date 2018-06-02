1647a811385e75b05f8e9d68059385fa2f147324
Canjiang Lu <craftsfish@163.com>
2018-06-02 17:18:33 +0800

    [retail]enabling filter of candidate items

--- vault/item_views.py
+++ vault/item_views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from .models import *
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.http import HttpResponse
+import json
 
 class ItemListView(ListView):
 	model = Item
@@ -17,6 +19,12 @@ class ItemListView(ListView):
 				candidates.add(Organization.objects.get(pk=o).root())
 			item.orgs = candidates
 		return context
+
+	def post(self, request, *args, **kwargs):
+		j = []
+		for i in Item.objects.filter(name__contains=request.POST['keyword']):
+			j.append({"id": i.id, "str": i.name})
+		return HttpResponse(json.dumps(j))
 
 class ItemDetailView(DetailView):
 	model = Item
