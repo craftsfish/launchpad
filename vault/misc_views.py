fb384739e89699dca3fe86984dd7920e08d0ccce
Canjiang Lu <craftsfish@163.com>
2018-06-09 15:13:01 +0800

    [change_repository]re-implemented

--- vault/misc_views.py
+++ vault/misc_views.py
@@ -12,6 +12,13 @@ class JdorderForm(forms.Form):
 	jdorder = forms.IntegerField()
 	organization = forms.ModelChoiceField(queryset=Organization.objects)
 
+class RetailForm(forms.Form):
+	organization = forms.ModelChoiceField(queryset=Organization.objects)
+	sale = forms.DecimalField(initial=0, max_digits=20, decimal_places=2)
+
+class ChangeForm(forms.Form):
+	organization = forms.ModelChoiceField(queryset=Organization.objects)
+
 class CommodityShippingBaseForm(forms.Form):
 	repository = forms.ModelChoiceField(queryset=Repository.objects, empty_label=None)
 	keyword = forms.CharField()
@@ -19,6 +26,13 @@ class CommodityShippingBaseForm(forms.Form):
 class CommodityShippingForm(forms.Form):
 	repository = forms.ModelChoiceField(queryset=Repository.objects, empty_label=None)
 	status = forms.ChoiceField(choices=Itemstatus.choices)
+	keyword = forms.CharField()
+
+class CommodityChangeRepositoryForm(forms.Form):
+	repository_f = forms.ModelChoiceField(queryset=Repository.objects, empty_label=None)
+	status_f = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.RadioSelect, initial=0)
+	repository_t = forms.ModelChoiceField(queryset=Repository.objects, empty_label=None)
+	status_t = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.RadioSelect, initial=0)
 	keyword = forms.CharField()
 
 class CommodityDetailBaseForm(forms.Form):
@@ -31,6 +45,16 @@ CommodityDetailBaseFormSet = formset_factory(CommodityDetailBaseForm, extra=0)
 class CommodityDetailForm(CommodityDetailBaseForm):
 	status = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.HiddenInput)
 CommodityDetailFormSet = formset_factory(CommodityDetailForm, extra=0)
+
+class CommodityChangeRepositoryDetailForm(forms.Form):
+	id = forms.IntegerField(widget=forms.HiddenInput)
+	quantity = forms.IntegerField()
+	check = forms.BooleanField(required=False)
+	repository_f = forms.ModelChoiceField(queryset=Repository.objects, widget=forms.HiddenInput)
+	status_f = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.HiddenInput)
+	repository_t = forms.ModelChoiceField(queryset=Repository.objects, widget=forms.HiddenInput)
+	status_t = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.HiddenInput)
+CommodityChangeRepositoryDetailFormSet = formset_factory(CommodityChangeRepositoryDetailForm, extra=0)
 
 class FfsMixin(ContextMixin):
 	"""
@@ -100,10 +124,6 @@ class JdorderMixin(FfsMixin):
 class DailyTaskView(TemplateView):
 	template_name = "{}/daily_task.html".format(Organization._meta.app_label)
 
-class RetailForm(forms.Form):
-	organization = forms.ModelChoiceField(queryset=Organization.objects)
-	sale = forms.DecimalField(initial=0, max_digits=20, decimal_places=2)
-
 class RetailView(FfsMixin, TemplateView):
 	template_name = "{}/retail.html".format(Organization._meta.app_label)
 	form_class = RetailForm
@@ -130,9 +150,6 @@ class RetailView(FfsMixin, TemplateView):
 		cash = Money.objects.get(name="人民币")
 		Transaction.add(self.task, "货款", t, self.org, cash.item_ptr, ("资产", "应收货款", None), self.sale, ("收入", "销售收入", None))
 		return super(RetailView, self).data_valid(form, formset)
-
-class ChangeForm(forms.Form):
-	organization = forms.ModelChoiceField(queryset=Organization.objects)
 
 class ChangeView(FfsMixin, TemplateView):
 	template_name = "{}/change.html".format(Organization._meta.app_label)
@@ -183,54 +200,26 @@ class JdorderReturnView(JdorderMixin, TemplateView):
 		Transaction.add(self.task, "退货", time, self.org, item, ("资产", status, repository), quantity, ("支出", "出货", repository))
 		return super(JdorderReturnView, self).formset_item_process(time, item, quantity, repository, status)
 
-class ChangeRepositoryForm(forms.Form):
-	organization = forms.ModelChoiceField(queryset=Organization.objects, empty_label=None)
-	repository_f = forms.ModelChoiceField(queryset=Repository.objects, widget=forms.RadioSelect, empty_label=None, initial=1)
-	status_f = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.RadioSelect, initial=0)
-	repository_t = forms.ModelChoiceField(queryset=Repository.objects, widget=forms.RadioSelect, empty_label=None, initial=1)
-	status_t = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.RadioSelect, initial=0)
-
-class ChangeRepositoryCommodityForm(forms.Form):
-	id = forms.IntegerField(widget=forms.HiddenInput)
-	quantity = forms.IntegerField()
-	check = forms.BooleanField(required=False)
-	repository_f = forms.ModelChoiceField(queryset=Repository.objects, widget=forms.HiddenInput)
-	status_f = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.HiddenInput)
-	repository_t = forms.ModelChoiceField(queryset=Repository.objects, widget=forms.HiddenInput)
-	status_t = forms.ChoiceField(choices=Itemstatus.choices, widget=forms.HiddenInput)
-ChangeRepositoryCommodityFormSet = formset_factory(ChangeRepositoryCommodityForm, extra=0)
-
-class ChangeRepositoryView(FormView):
+class ChangeRepositoryView(FfsMixin, TemplateView):
 	template_name = "{}/change_repository.html".format(Organization._meta.app_label)
-	form_class = ChangeRepositoryForm
+	form_class = ChangeForm
+	formset_class = CommodityChangeRepositoryDetailFormSet
+	sub_form_class = CommodityChangeRepositoryForm
 
-	def post(self, request, *args, **kwargs):
+	def data_valid(self, form, formset):
 		self.task = Task(desc="换仓")
 		self.task.save()
 		t = timezone.now()
-		form = ChangeRepositoryForm(self.request.POST)
-		if form.is_valid():
-			o = form.cleaned_data['organization']
-		formset = ChangeRepositoryCommodityFormSet(self.request.POST)
-		if formset.is_valid():
-			for f in formset:
-				d = f.cleaned_data
-				if d['check']:
-					c = Commodity.objects.get(pk=d['id'])
-					q = d['quantity']
-					if not q:
-						continue
-					rf = d['repository_f']
-					sf = Itemstatus.v2s(d['status_f'])
-					rt = d['repository_t']
-					st = Itemstatus.v2s(d['status_t'])
-					Transaction.add(self.task, "换仓", t, o, c.item_ptr, ("资产", sf, rf), -q, ("资产", st, rt))
-		return super(ChangeRepositoryView, self).post(request, *args, **kwargs)
-
-	def get_success_url(self):
-		return self.task.get_absolute_url()
-
-	def get_context_data(self, **kwargs):
-		context = super(ChangeRepositoryView, self).get_context_data(**kwargs)
-		context['formset'] = ChangeRepositoryCommodityFormSet(auto_id=False)
-		return context
+		o = form.cleaned_data['organization']
+		for f in formset:
+			d = f.cleaned_data
+			if not d['check']: continue
+			c = Commodity.objects.get(pk=d['id'])
+			q = d['quantity']
+			if not q: continue
+			rf = d['repository_f']
+			sf = Itemstatus.v2s(d['status_f'])
+			rt = d['repository_t']
+			st = Itemstatus.v2s(d['status_t'])
+			Transaction.add(self.task, "换仓", t, o, c.item_ptr, ("资产", sf, rf), -q, ("资产", st, rt))
+		return super(ChangeRepositoryView, self).data_valid(form, formset)
