98b4b02c4839fb892f97eff169d8999efbf54dac
Canjiang Lu <craftsfish@163.com>
2018-06-13 20:22:34 +0800

    [Turbine]generate candidates for replenish

--- vault/commodity_views.py
+++ vault/commodity_views.py
@@ -5,43 +5,13 @@ from django.views.generic import DetailView
 from django.db.models import Sum
 from django.utils import timezone
 from datetime import timedelta
+from turbine import *
 
 class CommodityListView(ListView):
 	model = Commodity
 
 class CommodityDetailView(DetailView):
 	model = Commodity
-
-	@staticmethod
-	def __get_shipping_out_information(commodity, repository, span):
-		r = []
-		speed = 0
-		active = 0
-		decay = 0.7
-		e = timezone.now().astimezone(timezone.get_current_timezone()).replace(hour=0, minute=0, second=0, microsecond = 0)
-		for i in range(span):
-			q = Split.objects.filter(account__item=commodity).filter(account__repository=repository).filter(account__name="出货")
-			q = q.filter(transaction__time__gte=(e-timedelta(1))).filter(transaction__time__lt=e)
-			v = q.aggregate(Sum('change'))['change__sum']
-			if v: v = int(v)
-			else: v = 0
-			r.append(v)
-			speed += decay ** i * (1 - decay) * v
-			if v: active += 1
-			e -= timedelta(1)
-		speed = speed * active / span
-		r.append(speed)
-		return r
-
-	@staticmethod
-	def __get_replenish_information(commodity, repository, speed, threshold):
-		inventory = Account.objects.filter(item=commodity).filter(repository=repository).filter(name__in=["完好", "应收"]).aggregate(Sum('balance'))['balance__sum']
-		if inventory: inventory = int(inventory)
-		else: inventory = 0
-		if speed <= 0:
-			return [8888, -inventory]
-		else:
-			return [inventory/speed, speed * threshold - inventory]
 
 	def get_context_data(self, **kwargs):
 		span = 10
@@ -69,12 +39,12 @@ class CommodityDetailView(DetailView):
 			l.append(total)
 
 			#shipment
-			l += CommodityDetailView.__get_shipping_out_information(self.object, r, span)
+			l += Turbine.get_shipping_out_information(self.object, r, span)
 
 			#storage
 			s = l[len(l)-1]
 			l.append(threshold)
-			l += CommodityDetailView.__get_replenish_information(self.object, r, s, threshold)
+			l += Turbine.get_replenish_information(self.object, r, s, threshold)
 
 		context['title'].append("合计")
 		e = timezone.now().astimezone(timezone.get_current_timezone()).replace(hour=0, minute=0, second=0, microsecond = 0)
--- vault/turbine.py
+++ vault/turbine.py
@@ -1,13 +1,60 @@
 # -*- coding: utf8 -*-
 from django.utils import timezone
 from datetime import timedelta
+from django.db.models import Sum
 from .models import *
 
 class Turbine:
 	@staticmethod
+	def get_shipping_out_information(commodity, repository, span):
+		r = []
+		speed = 0
+		active = 0
+		decay = 0.7
+		e = timezone.now().astimezone(timezone.get_current_timezone()).replace(hour=0, minute=0, second=0, microsecond = 0)
+		for i in range(span):
+			q = Split.objects.filter(account__item=commodity).filter(account__repository=repository).filter(account__name="出货")
+			q = q.filter(transaction__time__gte=(e-timedelta(1))).filter(transaction__time__lt=e)
+			v = q.aggregate(Sum('change'))['change__sum']
+			if v: v = int(v)
+			else: v = 0
+			r.append(v)
+			speed += decay ** i * (1 - decay) * v
+			if v: active += 1
+			e -= timedelta(1)
+		speed = speed * active / span
+		r.append(speed)
+		return r
+
+	@staticmethod
+	def get_replenish_information(commodity, repository, speed, threshold):
+		inventory = Account.objects.filter(item=commodity).filter(repository=repository).filter(name__in=["完好", "应收"]).aggregate(Sum('balance'))['balance__sum']
+		if inventory: inventory = int(inventory)
+		else: inventory = 0
+		if speed <= 0:
+			return [8888, -inventory]
+		else:
+			return [inventory/speed, speed * threshold - inventory]
+
+	@staticmethod
 	def replenish():
+		l = []
 		e = timezone.now().astimezone(timezone.get_current_timezone()).replace(hour=0, minute=0, second=0, microsecond = 0)
 		for c in Split.objects.filter(account__name="出货").filter(transaction__time__gte=(e-timedelta(28))).filter(transaction__time__lt=e).values_list('account__item', flat=True).distinct():
 			if Commodity.objects.filter(pk=c).exists():
 				c = Commodity.objects.get(pk=c)
-				print c
+				c.detail = []
+				need_refill = False
+				for r in Repository.objects.order_by("id"):
+					shipping = Turbine.get_shipping_out_information(c, r, 10)
+					speed = shipping[len(shipping)-1]
+					level, refill = Turbine.get_replenish_information(c, r, speed, 30)
+					if refill > 0:
+						need_refill = True
+					if refill != 0:
+						c.detail.append([r, level, refill])
+				if need_refill:
+					l.append(c)
+		for c in l:
+			for repo, level, refill in c.detail:
+				print "{}: {} | 库存天数: {} | 补仓数量: {}".format(c, repo, level, refill)
