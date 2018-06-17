d8c4961ca0ed98c6403d2e65b508172bad719e67
Canjiang Lu <craftsfish@163.com>
2018-06-17 10:20:48 +0800

    book transactions for wechat fake order

--- vault/jdorder.py
+++ vault/jdorder.py
@@ -102,6 +102,16 @@ class Jdorder(Task):
 					for t in o.task_ptr.transactions.filter(desc__contains=".出货."):
 						t.change_repository(o.repository, repo)
 
+				if o.task_ptr.transactions.filter(desc__startswith="微信刷单").exists() and not o.task_ptr.transactions.filter(desc="微信刷单.结算").exists():
+					cash = Money.objects.get(name="人民币")
+					a = Account.get(org.root(), cash.item_ptr, "负债", "应付账款", None)
+					b = Account.get(org, cash.item_ptr, "支出", "刷单", None)
+					Transaction.add(o.task_ptr, "微信刷单.结算", info.booktime, a, info.sale, b)
+					_org = Organization.objects.get(name="个人")
+					a = Account.get(_org, cash.item_ptr, "资产", "应收账款-为绿", None)
+					b = Account.get(_org, cash.item_ptr, "资产", "刷单资金", None)
+					Transaction.add(None, "微信刷单", info.booktime, a, info.sale, b)
+
 				o.status = Jdorder.str2status(info.status)
 				o.fake = f
 				o.repository = repo
