d5bac34040fe375a1a9c35b015ca1147127a513f
Canjiang Lu <craftsfish@163.com>
2018-06-17 10:39:40 +0800

    [itml]enable the book of wechat fake order

--- vault/tmorder.py
+++ vault/tmorder.py
@@ -71,6 +71,16 @@ class Tmorder(Task):
 					if original_repository.id != repository.id: #仓库发生变化
 						t.change_repository(original_repository, repository)
 
+				if o.task_ptr.transactions.filter(desc__startswith="微信刷单").exists() and not o.task_ptr.transactions.filter(desc="微信刷单.结算").exists():
+					cash = Money.objects.get(name="人民币")
+					a = Account.get(org.root(), cash.item_ptr, "负债", "应付账款", None)
+					b = Account.get(org, cash.item_ptr, "支出", "刷单", None)
+					Transaction.add(o.task_ptr, "微信刷单.结算", time, a, sale, b)
+					_org = Organization.objects.get(name="个人")
+					a = Account.get(_org, cash.item_ptr, "资产", "应收账款-腾复", None)
+					b = Account.get(_org, cash.item_ptr, "资产", "刷单资金", None)
+					Transaction.add(None, "微信刷单", time, a, sale, b)
+
 				o.status = Tmorder.str2status(status)
 				o.fake = fake
 				o.repository = repository
