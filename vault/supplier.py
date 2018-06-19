c0701f706a145073df992d9f637e4cbc4efe4977
Canjiang Lu <craftsfish@163.com>
2018-06-19 08:15:43 +0800

    [Supplier]+period for replenish

--- vault/supplier.py
+++ vault/supplier.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Supplier(models.Model):
 	name = models.CharField(max_length=30, unique=True)
+	period = models.IntegerField(default=15) #供货周期
 
 	def __str__(self):
 		return self.name
