9f60e76debe20fd8faa3917c48fa1786ceda84b0
Canjiang Lu <craftsfish@163.com>
2018-06-22 14:42:39 +0800

    trivial

--- vault/wallet_views.py
+++ vault/wallet_views.py
@@ -23,20 +23,21 @@ class WalletDetailView(ListView):
 		return ["%s/wallet_detail.html" % (Account._meta.app_label)]
 
 	def get_queryset(self):
-		self.object = Wallet.objects.get(pk=self.kwargs['pk'])
+		if not hasattr(self, "object"):
+			self.object = Wallet.objects.get(pk=self.kwargs['pk'])
 		return Split.objects.filter(account__name=self.object.name).order_by("-transaction__time", "-transaction__id", "-id")
 
 	def get_context_data(self, **kwargs):
 		context = super(WalletDetailView, self).get_context_data(**kwargs)
-		page = context['page_obj']
-		total = get_decimal_with_default(self.get_queryset()[:self.paginate_by * (page.number - 1)].aggregate(Sum("change"))['change__sum'], 0)
 
 		#object
 		context['object'] = self.object
 		self.object.balance = get_decimal_with_default(Account.objects.filter(name=self.object.name).aggregate(Sum('balance'))['balance__sum'], 0)
-		balance = self.object.balance - total
 
 		#detail
+		page = context['page_obj']
+		total = get_decimal_with_default(self.get_queryset()[:self.paginate_by * (page.number - 1)].aggregate(Sum("change"))['change__sum'], 0)
+		balance = self.object.balance - total
 		for s in context["object_list"]:
 			s.balance = balance
 			balance -= s.change
