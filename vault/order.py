d05cbe6a6c5913ddc5a10c3e0943e0cc41b4c4d4
Canjiang Lu <craftsfish@163.com>
2018-06-24 12:17:26 +0800

    [Order]delete obsolete transactions when necessary

--- vault/order.py
+++ vault/order.py
@@ -10,13 +10,19 @@ import re
 class Order(models.Model):
 	"""
 	[订单]
+	一个订单是一个Task，设计上应该继承Task模型。TODO: 能否迁移?
 	一个订单可以包含多条订购记录(invoice)，采用的是平台的商品编号，并且是一个无序的列表，同一平台商品可以出现多次。
-	系统内部对订单内的所有invoice按照平台编号，购买数量进行排序。
-	invoice永远存在，失效的invoice表示为出货+1&出货-1。TODO: 目前还不支持，优先级高，影响整个order的更新设计。
+	系统内部对订单的所有invoice按照平台编号，购买数量进行排序。
+	invoice永远存在，有以下几种状态 {
+		等待出库: (负债.应发, -1 | 支出.出货, +1)
+		已经出库: (资产.完好, -1 | 支出.出货, +1)
+		订购取消: (支出.出库, +1 | 支出.出货, +1)
+	}TODO: 目前还不支持，优先级高，影响整个order的更新设计。
 	一个平台商品可以对应物理上的多个商品，内部通过构建相应的映射表，来描述两者之间的关系。
 	每个映射有时间戳表示生效时间，这样的设计允许平台商品在不同的时间段映射不同的物理商品组合。
 	系统内部标识一个具体的物理出货记录时，transaction的desc属性格式为{invoice排序号}.出货.{平台编号}.{商品名称}
 	TODO: 这里的商品名称是否必要，因为每个transacton对应的交易标的已经包含了该信息？
+	TODO: transaction的desc的格式修改为出货.{invoice排序号}.{平台编号}，这样方便生成订单内的其他关联交易。
 
 	[刷单]
 	1: 空包
@@ -32,7 +38,7 @@ class Order(models.Model):
 	刷单.发货 | 刷单.发货 | (根据具体情况的不同，刷单需要发出的货物，比如洗衣粉，肥皂，盐等)
 	刷单.结算 | 刷单.结算 | (刷单的费用，实际订单费用)
 	刷单.佣金 | 刷单.拥挤 | (刷单的费用，佣金)
-	退货 | 退货 | (正常退货，对应的invoice状态为完结，则记录为一条收货记录。状态为删除或者取消则记录为一条换仓记录。TODO：这个规则目前还没有应用)
+	退货 | 退货 | (正常退货，对应的invoice状态为完成，则记录为一条收货记录。状态为删除或者取消则记录为一条换仓记录。TODO：这个规则目前还没有应用)
 	换货.收货 | 换货.收货 | (订单换货)
 	换货.发货 | 换货.发货 | (订单换货)
 	结算 | 结算 | (订单费用结算)
@@ -44,6 +50,8 @@ class Order(models.Model):
 	time = models.DateTimeField(default=timezone.now)
 	repository = models.ForeignKey(Repository, null=True, blank=True, related_name="%(app_label)s_%(class)s_order_set", related_query_name="%(app_label)s_%(class)s", verbose_name="发货仓库")
 	sale = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+
+	#以下属于仅适用于刷单
 	counterfeit = models.ForeignKey(Counterfeit, verbose_name="刷单平台", null=True, blank=True)
 	delivery = models.BooleanField("真实发货", default=False)
 	recall = models.BooleanField("实物回收", default=False)
@@ -152,20 +160,26 @@ class Order(models.Model):
 		first_shipment = task.transactions.filter(desc__startswith="1.出货.").first()
 		if not first_shipment:
 			return
-
 		shipout_split = first_shipment.splits.filter(account__category=Account.str2category("支出")).get(change__gt=0)
 		original_repository = shipout_split.account.repository
 		organization = shipout_split.account.organization
+
 		#出货
 		Order.delivery_repository_update(task, original_repository, self.repository)
+
 		#刷单.回收
 		if self.counterfeit and not self.counterfeit.delivery:
-					if not task.transactions.filter(desc__contains="刷单.回收").exists():
-						Order.fake_recall_create(task)
-					else:
-						self.fake_recall_update()
+			if not task.transactions.filter(desc__contains="刷单.回收").exists():
+				Order.fake_recall_create(task)
+			else:
+				self.fake_recall_update()
+		else:
+			task.delete_transactions_contains_desc("刷单.回收")
+
 		#刷单.结算
-		if self.counterfeit and not task.transactions.filter(desc="刷单.结算").exists():
+		if not self.counterfeit:
+			task.delete_transactions_contains_desc("刷单.结算")
+		elif not task.transactions.filter(desc="刷单.结算").exists():
 			fake_deliver = task.transactions.filter(desc="刷单.发货").first()
 			if fake_deliver:
 				when = fake_deliver.time
@@ -175,4 +189,5 @@ class Order(models.Model):
 			a = Account.get(organization, cash.item_ptr, "支出", "{}刷单".format(self.counterfeit), None)
 			b = Account.get(organization.root(), cash.item_ptr, "资产", "运营资金.微信", None) #TODO:取决于具体的情况
 			Transaction.add(task, "刷单.结算", when, a, self.sale, b)
-			#TODO, 更新
+		else:
+			pass #TODO, 更新
