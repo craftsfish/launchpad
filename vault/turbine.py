a13c6437d0d515c097c37f8eb27818b34bb2a9a1
Canjiang Lu <craftsfish@163.com>
2018-06-24 12:19:41 +0800

    clean up

--- vault/turbine.py
+++ vault/turbine.py
@@ -189,10 +189,3 @@ class Turbine:
 		)
 		for n, d, r in counterfeits:
 			Counterfeit.objects.get_or_create(name=n, delivery=d, recall=r)
-
-	@staticmethod
-	def wechat_fake_clear(organization, task, time, bill):
-		cash = Money.objects.get(name="人民币")
-		a = Account.get(organization, cash.item_ptr, "支出", "微信刷单", None)
-		b = Account.get(organization.root(), cash.item_ptr, "资产", "运营资金.微信", None)
-		Transaction.add(task, "刷单.结算", time, a, bill, b)
