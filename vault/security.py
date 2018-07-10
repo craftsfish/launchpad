b0730da9bc1920bf13cddfa772282852dcd3c21a
Canjiang Lu <craftsfish@163.com>
2018-07-10 16:43:51 +0800

    [navbar]+more security options

--- vault/security.py
+++ vault/security.py
@@ -7,9 +7,18 @@ class SecurityLoginRequiredMixin(LoginRequiredMixin):
 	login_url = '/login/'
 
 	def get_context_data(self, **kwargs):
-		if 'nav_items' not in kwargs:
+		m = (
+			("修改密码", reverse('password_change')),
+			("退出", reverse('logout')),
+		)
+		l = []
+		for name, url in m:
 			i = NavItem()
-			i.name = "退出"
-			i.url = reverse('logout')
-			kwargs['nav_items'] = [i]
+			i.name = name
+			i.url = url
+			l.append(i)
+		if 'nav_items' not in kwargs:
+			kwargs['nav_items'] = l
+		else:
+			kwargs['nav_items'] += l
 		return super(SecurityLoginRequiredMixin, self).get_context_data(**kwargs)
