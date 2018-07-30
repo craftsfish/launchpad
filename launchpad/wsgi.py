221d1d32e7b92fed8b471c59158bb8ff30502747
Canjiang Lu <craftsfish@163.com>
2018-07-30 11:32:43 +0800

    [UnicodeEncodingError]a quick solution

--- launchpad/wsgi.py
+++ launchpad/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "launchpad.settings")
 
 application = get_wsgi_application()
