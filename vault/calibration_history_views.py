1063885f068d24450ef20ab97f1617ffa7fad7d6
Canjiang Lu <craftsfish@163.com>
2018-11-08 15:44:53 +0800

    trivial

--- vault/calibration_history_views.py
+++ vault/calibration_history_views.py
@@ -7,3 +7,9 @@ from calibration_history import *
 class CalibrationHistoryListView(ListView):
 	model = CalibrationHistory
 	paginate_by = 20
+
+	def get_context_data(self, **kwargs):
+		context = super(CalibrationHistoryListView, self).get_context_data(**kwargs)
+		for o in context['object_list']:
+			o.utc_human = datetime.fromtimestamp(o.utc)
+		return context
