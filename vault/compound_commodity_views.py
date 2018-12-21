4a74c0bb1054e0c13a79d8d10db436537e09fa21
Canjiang Lu <craftsfish@163.com>
2018-12-21 11:34:19 +0000

    [CompoundCommodityDetailView]done

--- vault/compound_commodity_views.py
+++ vault/compound_commodity_views.py
@@ -12,9 +12,28 @@ from .security import *
 class CompoundCommodityListView(SecurityLoginRequiredMixin, ListView):
 	model = CompoundCommodity
 
+def shipping_info_of_commodities(commodities, end, span):
+	result = []
+	for i in range(span):
+		q = Split.objects.filter(account__item__in=commodities).filter(account__organization=Organization.objects.get(name='泰福高腾复专卖店')).filter(account__name="出货")
+		q = q.filter(transaction__time__gte=(end-timedelta(1))).filter(transaction__time__lt=end)
+		v = q.aggregate(Sum('change'))['change__sum']
+		v = get_int_with_default(v, 0)
+		q = q.filter(transaction__desc__contains='刷单.回收')
+		w = q.aggregate(Sum('change'))['change__sum']
+		w = -get_int_with_default(w, 0)
+		end -= timedelta(1)
+		result.append([end, v, w])
+	return result
+
 class CompoundCommodityDetailView(DetailView):
 	model = CompoundCommodity
 
 	def get_context_data(self, **kwargs):
 		context = super(CompoundCommodityDetailView, self).get_context_data(**kwargs)
+		obj = context['object']
+		info = shipping_info_of_commodities(obj.commodities.all(), begin_of_day(), 30)
+		for i, j, k in info:
+			print "{}: 出货: {}, 刷单: {}".format(i, j, k)
+		context['info'] = info
 		return context
