a69529bf5cc7d670138de3e841b6d753b13a2b9b
Canjiang Lu <craftsfish@163.com>
2018-12-25 07:41:53 +0000

    trivial

--- vault/customer_views.py
+++ vault/customer_views.py
@@ -5,7 +5,7 @@ from .security import *
 
 class CustomerListView(SecurityLoginRequiredMixin, ListView):
 	model = Customer
-	paginate_by = 50
+	paginate_by = 1
 
 	def get_queryset(self):
 		#if self.kwargs['key'] == 0:
@@ -19,7 +19,13 @@ class CustomerListView(SecurityLoginRequiredMixin, ListView):
 			if t.counterfeit:
 				t.flag = "刷手"
 			t.join = utc_2_datetime(t.join).astimezone(timezone.get_current_timezone())
+			t.recruited = True
+			if t.recruit == 0:
+				t.recruited = False
 			t.recruit = utc_2_datetime(t.recruit).astimezone(timezone.get_current_timezone())
+			t.jdorders = Jdorder.objects.filter(contact__in=t.contact_set.all()).all()
+			t.tmorders = Tmorder.objects.filter(contact__in=t.contact_set.all()).all()
+			def __key(o):
+				return o.time
+			t.orders = sorted(list(t.jdorders) + list(t.tmorders), key=__key)
 		return context
-
-
