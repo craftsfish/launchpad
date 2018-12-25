91ab6da4ec1fb0f33401e2df4f7da20cf5a93de6
Canjiang Lu <craftsfish@163.com>
2018-12-25 08:01:38 +0000

    trivial

--- vault/customer_views.py
+++ vault/customer_views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .models import *
 from django.views.generic import ListView
+from django.views.generic import RedirectView
 from .security import *
 
 class CustomerListView(SecurityLoginRequiredMixin, ListView):
@@ -9,7 +10,7 @@ class CustomerListView(SecurityLoginRequiredMixin, ListView):
 
 	def get_queryset(self):
 		#if self.kwargs['key'] == 0:
-		return Customer.objects.order_by("counterfeit", 'recruit', 'join')
+		return Customer.objects.order_by('recruit', 'counterfeit', 'join')
 		#return Customer.objects.filter(contact__phone='19971579166').order_by("counterfeit", 'recruit', 'join')
 
 	def get_context_data(self, **kwargs):
@@ -23,9 +24,21 @@ class CustomerListView(SecurityLoginRequiredMixin, ListView):
 			if t.recruit == 0:
 				t.recruited = False
 			t.recruit = utc_2_datetime(t.recruit).astimezone(timezone.get_current_timezone())
+			t.contacts = t.contact_set.all()
 			t.jdorders = Jdorder.objects.filter(contact__in=t.contact_set.all()).all()
 			t.tmorders = Tmorder.objects.filter(contact__in=t.contact_set.all()).all()
 			def __key(o):
 				return o.time
 			t.orders = sorted(list(t.jdorders) + list(t.tmorders), key=__key)
 		return context
+
+class CustomerRecruitView(RedirectView):
+	def get_redirect_url(self, *args, **kwargs):
+		return reverse('customer_list')
+
+	def get(self, request, *args, **kwargs):
+		i = kwargs['uuid']
+		c = Customer.objects.get(uuid=i)
+		c.recruit = now_as_seconds()
+		c.save()
+		return super(CustomerRecruitView, self).get(request, *args, **kwargs)
