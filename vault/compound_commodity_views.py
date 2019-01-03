8767c70b3d06fb9b0a32d49f3d446651fa06de5b
Canjiang Lu <craftsfish@163.com>
2019-01-03 07:23:54 +0000

    add place info of compoundcommodity

--- vault/compound_commodity_views.py
+++ vault/compound_commodity_views.py
@@ -26,14 +26,45 @@ def shipping_info_of_commodities(commodities, end, span):
 		result.append([end, v, w])
 	return result
 
+def place_info_of_commodities(commodities, end, span):
+	info = []
+	result = {}
+	q = Split.objects.filter(account__item__in=commodities).filter(account__organization=Organization.objects.get(name='泰福高腾复专卖店')).filter(account__name="出货")
+	q = q.filter(transaction__time__gte=(end-timedelta(span))).filter(transaction__time__lt=end)
+	for s in q:
+		if s.transaction.task.tmorder.counterfeit != None:
+			continue
+		a = s.transaction.task.tmorder.address
+		key = a.parent.id
+		if result.get(key) == None:
+			result[key] = 0
+		result[key] += s.change
+
+	def __key(i):
+		k, v = i
+		a = Address.objects.get(id=k)
+		if a.level == 1:
+			return a.parent.id * 100000 + a.id
+		else:
+			return a.id * 100000
+	for k, v in sorted(result.items(), key=__key):
+		a = Address.objects.get(id=k)
+		s = a.name
+		if a.level == 1:
+			s = a.parent.name + a.name
+		info.append([s, v])
+	return info
+
 class CompoundCommodityDetailView(DetailView):
 	model = CompoundCommodity
 
 	def get_context_data(self, **kwargs):
 		context = super(CompoundCommodityDetailView, self).get_context_data(**kwargs)
 		obj = context['object']
+		place = place_info_of_commodities(obj.commodities.all(), begin_of_day(), 30)
 		info = shipping_info_of_commodities(obj.commodities.all(), begin_of_day(), 30)
 		for i, j, k in info:
 			print "{}: 出货: {}, 刷单: {}".format(i, j, k)
 		context['info'] = info
+		context['place'] = place
 		return context
